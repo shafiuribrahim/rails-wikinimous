921548501d2bc2c0a21df308b72cc39935a1b27f
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-02-25 16:02:22 +0800

    created a wiki like page with faker gem and crud basics

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'faker'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -39,6 +40,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# tour_d_argent = Restaurant.new(name: "La Tour d'Argent")
+# tour_d_argent.save!
+
+# chez_gladines = Restaurant.new(name: "Chez Gladines")
+# chez_gladines.save!
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quotes::Shakespeare.as_you_like_it_quote
+  )
+  article.save!
+end
